b7cb3931a17adc943d0d25ac23515a6840b7612e
Ben Godfrey <benjetson5236@gmail.com>
2016-12-17 17:45:12 -0500

    Added initial codebase.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# starstruck-A
+# Team 3796A: VRC Starstruck Code
+This repo houses the code for our 2016-17 VRC Starstruck robot.
---
+++ robot_constants.h
@@ -0,0 +1,12 @@
+#ifndef ROBOT_CONSTANTS_H
+#define ROBOT_CONSTANTS_H
+
+    const char MOTOR_MAX = 127;
+    const char MOTOR_HALF = MOTOR_MAX/2;
+    const char FLYWHEEL_DEFAULT = 50;
+    char FLYWHEEL_SPEED = 50;
+    const char AUTON_FLYWHEEL_SPEED = 55;
+    const char MOTOR_OFF_COAST = 1;
+    const char MOTOR_OFF_HARD = 0;
+
+#endif
