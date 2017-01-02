e2bc672b69d40e9f96d6b320a6e5f8d59b89adf1
Ben Godfrey <BenJetson@users.noreply.github.com>
2017-01-02 13:50:51 -0500

    invert axes for more logical control

--- TESTING.c
+++ TESTING.c
@@ -19,14 +19,14 @@ void driveControl() {
 
   //Create "deadzone" for Y1/Ch4
 	if(abs(vexRT[Ch4]) > threshold) {
-		Y1 = vexRT[Ch4];
+		Y1 = -1 * vexRT[Ch4];
 	}	else {
 		Y1 = 0;
 	}
 
 	//Create "deadzone" for X1/Ch3
 	if(abs(vexRT[Ch3]) > threshold) {
-		X1 = vexRT[Ch3];
+		X1 = -1 * vexRT[Ch3];
 	}	else {
 		X1 = 0;
 	}
